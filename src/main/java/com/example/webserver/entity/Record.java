764d2a4b446177762ce4df1e540ecf1b0f5ce8d8
zyerin <zyerin5974@gmail.com>
2024-11-24 04:26:55 +0900

    feat: Record 추가 - 알림 확인 유무 checked 추가

--- src/main/java/com/example/webserver/entity/Record.java
+++ src/main/java/com/example/webserver/entity/Record.java
@@ -1,2 +1,35 @@
-package com.example.webserver.entity;public class Record {
+package com.example.webserver.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Document(collection = "record")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Record {
+    @Id
+    private String id;
+
+    private String memberId;
+
+    private String deviceType;
+
+    private LocalDateTime time;
+
+    private boolean checked;    // 알림 확인 유무
+
+    public Record(String memberId, String deviceType, LocalDateTime time) {
+        this.memberId = memberId;
+        this.deviceType = deviceType;
+        this.time = time;
+        this.checked = false;
+    }
 }
