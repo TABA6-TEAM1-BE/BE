71f93c6bffa87e552e61d63e3736937a9ed5d956
zyerin <zyerin5974@gmail.com>
2024-11-24 04:29:58 +0900

    feat: RecordRepository 코드 추가 - 미확인 알림/날짜별, 제품별 Record 조회

--- src/main/java/com/example/webserver/repository/RecordRepository.java
+++ src/main/java/com/example/webserver/repository/RecordRepository.java
@@ -14,6 +14,6 @@ public interface RecordRepository extends MongoRepository<Record, String> {
     // checked 가 false 인 Record 반환 -> 클라이언트가 아직 확인하지 못한 알림
     public List<Record> findAllByMemberIdAndCheckedIsFalse(String memberId);
 
-    // 해당 날짜의 deviceType 별 Record 조회
+    // 해당 날짜의 deviceType 별로 Record 조회
     List<Record> findAllByMemberIdAndDeviceTypeAndTimeBetween(String memberId, String deviceType, LocalDateTime startDate, LocalDateTime endDate);
 }
