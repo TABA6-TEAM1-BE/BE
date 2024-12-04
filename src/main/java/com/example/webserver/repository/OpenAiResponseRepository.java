588e941a7e23b2636735d35520d771c20a4a9c79
zyerin <zyerin5974@gmail.com>
2024-12-04 12:08:38 +0900

    feat: AiResultController 코드 추가 - isHuman, text

--- src/main/java/com/example/webserver/repository/OpenAiResponseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.webserver.repository;
-
-import com.example.webserver.entity.OpenAiResponseDocument;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OpenAiResponseRepository extends MongoRepository<OpenAiResponseDocument, String> {
-}
