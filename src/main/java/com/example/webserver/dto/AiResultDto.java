3b5ace17402a0e0cc2e61e69ad80f38236f72f7e
zyerin <zyerin5974@gmail.com>
2024-12-04 12:09:57 +0900

    feat: AiResultDto 코드 추가 - iHuman, text

--- src/main/java/com/example/webserver/dto/AiResultDto.java
+++ src/main/java/com/example/webserver/dto/AiResultDto.java
@@ -9,5 +9,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class AiResultDto {
     private String recordIdx;
-    private String result;
+    private String result;  // ai 모델 결과값
+
+    private Boolean isHuman = false;
+
+    private String text = null; // isHuman 일 경우 stt 결과값 저장
 }
