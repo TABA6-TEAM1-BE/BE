44606a68c89809577de43ab79565d31096fdc0a0
zyerin <zyerin5974@gmail.com>
2024-12-04 10:52:41 +0900

    refact: Record 엔티티에 memberId 대신 UserIdx 추가 -> 그에 따라 Controller, Repository, Service 코드 수정

--- src/main/java/com/example/webserver/controller/AiResultController.java
+++ src/main/java/com/example/webserver/controller/AiResultController.java
@@ -1,16 +1,20 @@
 package com.example.webserver.controller;
 
 import com.example.webserver.dto.AiResultDto;
+import com.example.webserver.entity.Record;
 import com.example.webserver.service.RecordService;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/ai")
@@ -24,16 +28,17 @@ public class AiResultController {
     @PostMapping("/results")
     public ResponseEntity<String> receiveAIResult(@RequestBody AiResultDto resultDto) {
         try {
-            String userIdx = resultDto.getUserIdx(); // 사용자 식별
-            String recordIdx = resultDto.getRecordIdx();    // 기록 식별
+            String recordIdx = resultDto.getRecordIdx();  // 기록 식별
             String result = resultDto.getResult();  // AI 결과값
 
             // Record 업데이트 (resultTime 및 deviceType 저장)
-            boolean isUpdated = recordService.updateRecordWithAIResult(recordIdx, result);
-            if (!isUpdated) {
-                log.warn("Record with recordIdx {} not found", recordIdx);
-                return ResponseEntity.status(404).body("Record not found");
+            if (!recordService.updateRecordWithAIResult(recordIdx, result)) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Record with recordIdx " + recordIdx + " not found.");
             }
+
+            // recordIdx 에 해당하는 기록의 userIdx 찾기 -> websocket 의 경로에 필요
+            Optional<Record> optionalRecord = recordService.getRecordByRecordIdx(recordIdx);
+            String userIdx = optionalRecord.get().getUserIdx();
 
             // WebSocket 경로로 메시지 전송
             String destination = "/socket/" + userIdx;
